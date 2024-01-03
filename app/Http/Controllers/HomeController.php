12a1d62c13172213c3c28211873ede34fff98c74
Huỳnh Đức <duc.huynh25@student.passerellesnumeriques.org>
2024-01-03 23:48:28 +0700

    bestseller: 60%

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use App\Services\Interfaces\IProductService;
 use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
 
--- resources/views/pages/bestseller/index.blade.php
+++ resources/views/pages/bestseller/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('title')
 @section('css')
-  <link rel="stylesheet" href="/assets/css/home.css">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="/assets/css/bestseller.css">
 @endsection
 @section('content')
@@ -10,71 +10,37 @@
           echo '
       <div class="container-fluid" id="borderHr">
           <div class="row d-flex text-center">
-                  <div class="col ml-2"><hr id="hr"></div>
+                  <div class="col ml-2"><hr id="hr"></div>         
                   <div class="col">
-                      <h2>' . $text . '</h2>
+                      <h2 class="h2Title">' . $text . '</h2>
                   </div>
                   <div class="col mr-2"><hr id="hr"></div>
                 </div>
       </div>
       ';
-      }
-      function generateProductCard($id,$name,$size,$price,$description,$category,$image,$discount) {
-        echo '
-                <div class="card mr-3 mb-3">
-                    <div class="circle-container1">
-                        <span>50 % OFF</span>
-                    </div>
-                    <img class="img"
-                        src="' . $image . '"
-                        alt="a">
-                    <div class="title">' . $name . '</div>
-                    <div class="rating">
-                        <input value="5" name="rate" id="star5" type="radio">
-                        <label title="text" for="star5"></label>
-                        <input value="4" name="rate" id="star4" type="radio">
-                        <label title="text" for="star4"></label>
-                        <input value="3" name="rate" id="star3" type="radio" checked="">
-                        <label title="text" for="star3"></label>
-                        <input value="2" name="rate" id="star2" type="radio">
-                        <label title="text" for="star2"></label>
-                        <input value="1" name="rate" id="star1" type="radio">
-                        <label title="text" for="star1"></label>
-                        <span class="ratingTotal">(' . 999 . ')</span>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="oldPrice">
-                                $' . $price . '
-                                <hr class="removePrice">
-                            </div>
-                        </div>
-                        <div class="col"></div>
-                        <div class="col ">
-                            <span class="newPrice">$' . $price- ($price * ($discount/100)) . '</span>
-                        </div>
-                        <div class="col">
-                        </div>
-                    </div>
-                    <div class="size_product">
-                        <b>Size: </b>
-                        <select name="product_size" id="product_size">
-                            <option value="'. $size .'">'.$size .'ml</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <button class="btn mb-3" id="buynowBtn" onclick="redirectToDetail(' . $id . ')">Buy Now</button>
-                        </div>
-                    </div>
+      }   
+      function generateDiscountHeader2($text) {
+          echo '
+      <div class="container-fluid" id="borderHr">
+          <div class="row d-flex text-center">
+                  <div class="col ml-2"><hr id="hr"></div>
+                  <div class="col-1">
+                        <img src="https://res.cloudinary.com/duas1juqs/image/upload/v1704266655/Web%20DDA%20COMECTIC/iconBestSeller_quuaqq.png" alt="icon">
+                  </div>
+                  <div class="col">
+                      <h2>' . $text . '</h2>
+                  </div>
+                  <div class="col-1">
+                    <img src="https://res.cloudinary.com/duas1juqs/image/upload/v1704266655/Web%20DDA%20COMECTIC/iconBestSeller_quuaqq.png" alt="icon">
+                  </div>
+                  <div class="col mr-2"><hr id="hr"></div>
                 </div>
-            ';
-    } 
-    
-    
+      </div>
+      ';
+      }   
     function CardBestller($id,$name,$size,$price,$description,$category,$image,$discount) {
         echo '
-                <div class="card mr-3 mb-3">
+                <div class="card" style="border:0px">
                     <div class="love-container">
                         <i class="fa-solid fa-heart"></i>
                     </div>
@@ -112,7 +78,7 @@
                         <select name="product_size" id="product_size">
                             <option value="'.$size.'">'.$size .'ml</option>
                         </select>
-                        <label class="user-label">First Name</label>
+                        <button class="user-label">Select the size</button>
                     </div>
                     <div class="row">
                         <div class="col">
@@ -142,10 +108,36 @@
             window.location.href = '/detailproduct?id=' + productId;
         }
    </script>
+    <?php generateDiscountHeader2("SKINCARE TOP SELLER") ?>
+    <div class="row mx-4">
+        <div class="col-3 bg-light" style="max-height:600px">
+           <center> <button class="btn fillter mt-4" id="fillter">FILLTER BY</button></center>
+        </div>
+        <div class="col-9">
+            <div class="row">
+                @foreach ($products as $product)
+                    <div class="col ml-2 mt-4">
+                        @php 
+                            CardBestller(
+                                $product->id,
+                                $product->product_name,
+                                $product->size,
+                                $product->price,
+                                $product->description,
+                                $product->category,
+                                $product->image,
+                                50,
+                            );
+                        @endphp
+                    </div>
+                    @if ($loop->iteration % 4 == 0)
+                        </div><div class="row">
+                    @endif
+                @endforeach
+            </div>
+        </div>        
+    </div>
+    <?php generateDiscountHeader("YOU MAY ALSO LIKE") ?>
 
-    <h2>Đây là BestSeller</h2>
-    <?php   
-    CardBestller("hahahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","hahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",1,1,1,1,'https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/2c/58/35/2c583588-177a-0f1f-21a7-91f9789d3cf2/8809603547668_Cover.jpg/1200x1200bf-60.jpg','1')
-    ?>
     <?php generateDiscountHeader("ABOUT US") ?>
 @endsection

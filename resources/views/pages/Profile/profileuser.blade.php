6f16ad4740073399b39576738001fbeb87c3ea09
Huỳnh Đức <duc.huynh25@student.passerellesnumeriques.org>
2024-01-18 09:36:17 +0700

    save

--- resources/views/pages/Profile/profileuser.blade.php
+++ resources/views/pages/Profile/profileuser.blade.php
@@ -56,8 +56,8 @@
                             <input class="d-none" type="text" name="id" value="{{ session('user_data')['id'] }}">
                             <div class="form-group">
                                 <label for="editProfileImage">Profile Image</label>
-                                <input type="file" class="form-control-file" id="editProfileImage"
-                                    name="editProfileImage" value="{{ session('user_data')['image'] }}">
+                                <input type="file" id="fileInput" />
+                                <p id="uploading_text"></p> 
                             </div>
                             <div class="form-group">
                                 <label for="last_name">Last Name</label>
@@ -105,4 +105,47 @@
             return redirect('/login');
         @endphp
     @endif
+
+    <script>
+        //Code HTML 👇
+        // <input type="file" id="fileInput" />
+        // <p id="uploading_text"></p> 
+//Code HTML 👆
+        const fileInput = document.getElementById("fileInput");
+        const uploading_text = document.getElementById("uploading_text");
+
+// // replace with your data 👇
+        const cloud_name = "duas1juqs";
+        const upload_preset = "pnvimage";
+// // replace with your data 👆
+
+        fileInput.addEventListener("change", (e) => {
+        uploading_text.innerText = "uploading...";
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("upload_preset", upload_preset);
+        const options = {
+            method: "POST",
+            body: formData,
+        };
+
+        return fetch(
+            `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+            options
+        )
+            .then((res) => res.json())
+            .then((data) => {
+            console.log(data.secure_url);
+
+            uploading_text.innerHTML = `
+            <span>upload complete.</span>
+            <br />
+            <img style="max-width:300px" src="${data.secure_url}" alt="">
+            <a href="${data.secure_url}">${data.secure_url}</a>
+            `;
+            })
+            .catch((err) => console.log(err));
+        });
+    </script>
 @endsection
